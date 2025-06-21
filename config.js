341be412e5b3b4386c172245cba343b5f97cd49e
Omotayo25 <lolaogunesan@yahoo.com>
2025-06-21 19:31:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ const toBool = (value) => value === "true";
 // Owner & Bot Details
 global.owner = process.env.OWNER_NUMBER;
 global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.ownername = process.env.OWNER_NAME || "Plutomoney";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.author = process.env.AUTHOR || "Plutomoney";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
 
